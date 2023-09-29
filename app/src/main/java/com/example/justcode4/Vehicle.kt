b6b8139d2df7e22e1ce5a36d07dd37430d1c66cd
Togzhan Kali <t_kali@kbtu.kz>
2023-09-30 04:15:58 +0600

    Init commit

---
+++ app/src/main/java/com/example/justcode4/Vehicle.kt
@@ -0,0 +1,11 @@
+package com.example.justcode4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Vehicle : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vehicle)
+    }
+}
